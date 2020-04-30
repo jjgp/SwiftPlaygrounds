042fe98cfca458c04b934d806f804ad377b5a86d
Jason Prasad <jasongprasad@gmail.com>
2020-04-29 21:04:44 -0400

    Let's Play
    Starting with RxSwift

---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+target :RxSwiftTests do
+  platform :ios, '13.0'
+  use_frameworks!
+  pod 'RxBlocking'
+  pod 'RxCocoa'
+  pod 'RxSwift'
+  pod 'RxTest'
+end
---
+++ RxSwiftTests/RxSwiftTests.swift
@@ -0,0 +1,14 @@
+//
+//  RxSwiftTests.swift
+//  RxSwiftTests
+//
+//  Created by Jason Prasad on 4/29/20.
+//
+
+import RxBlocking
+import RxCocoa
+import RxSwift
+import RxTest
+import XCTest
+
+class RxSwiftTests: XCTestCase {}
